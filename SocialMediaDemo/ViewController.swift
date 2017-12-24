53c7a1a915c8768317592b748d7fe7df7fe9c325
Ulices Meléndez Acosta <ulices009@hotmail.com>
2017-12-24 12:09:54 -0500

    Commit inicial

---
+++ SocialMediaDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialMediaDemo
+//
+//  Created by Ulices Meléndez on 24/12/17.
+//  Copyright © 2017 Ulices Meléndez Acosta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
